20229b6f170c146db500ebaaf571087a7c31bd02
Tomáš Malínský <tmal@malanak.cz>
2026-02-15 11:16:56 +0100

    configure treesitter + remove lockfile

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,15 @@
-return { "https://github.com/nvim-treesitter/nvim-treesitter", branch = "master" }
+return {
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "go",
+      },
+      highlight = {
+        enable = true,
+      },
+    })
+  end,
+}
